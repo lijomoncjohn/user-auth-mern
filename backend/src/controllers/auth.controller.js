41d1b37d3d429fca57c1560cd54b9c748dfa1da3
Lijomon C John <lijomon@promasoft.net>
2021-08-23 21:55:31 +0530

    Login API

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,34 @@
+const User = require("../models/user.model");
+const ErrorResponse = require("../utils/errorResponse");
+
 exports.login = async (req, res, next) => {
-    res.sendStatus(200)
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        return next(new ErrorResponse(`User not found with email ${email}`, 400));
+    }
+
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+        return res.status(403).josn({
+            success: false,
+            message: 'Invalid password',
+        })
+    }
+
+    const token = user.genrateToken();
+
+    return res.status(200).json({
+        success: true,
+        message: 'Loggin success',
+        data: {
+            token,
+            userId: user._id,
+        },
+    });
 }
 
 exports.register = async (req, res, next) => {

85732812599fc95e2f353e793c4763727c298a78
Lijomon C John <lijomon@promasoft.net>
2021-08-24 11:40:45 +0530

    Form validations

--- frontend/src/containers/Auth/Login.js
+++ frontend/src/containers/Auth/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import { Formik } from 'formik'
 
 import FormGroup from '../../components/Form/FormGroup';
 import FormLabel from '../../components/Form/FormLabel';
@@ -8,10 +9,15 @@ import Button from '../../components/UI/Button/Button';
 import Card from '../../components/UI/Card/Card';
 import CardBody from '../../components/UI/Card/CardBody';
 import CardHeader from '../../components/UI/Card/CardHeader';
+import ErrorText from '../../components/Form/ErrorText'
 
 import './Login.css';
 
 const Login = () => {
+	const handleLogin = (values, { setSubmitting }) => {
+		alert(JSON.stringify(values))
+	}
+
 	return (
 		<div className='section-login'>
 			<Card className='card-login'>
@@ -19,33 +25,70 @@ const Login = () => {
 					<h5 className='font-weight-bold'>Log In</h5>
 				</CardHeader>
 				<CardBody>
-					<form>
-						<FormGroup>
-							<FormLabel label='Email address' />
-							<TextInput
-								type='email'
-								name='email'
-							/>
-						</FormGroup>
-						<FormGroup>
-							<FormLabel label='Password' />
-							<TextInput
-								type='password'
-								name='password'
-							/>
-						</FormGroup>
-						<div className='d-flex justify-content-between'>
-							<Link to={'/register'} className='mt-2'>
-								Forgot password?
-							</Link>
-							<Button className='btn-primary btn-success px-3' title='Login' />
-						</div>
-						<div className='d-flex justify-content-start mt-3'>
-							<Link to={'/register'}>
-								Register Now
-							</Link>
-						</div>
-					</form>
+					<Formik
+						initialValues={{ email: '', password: '' }}
+						validate={(values) => {
+							const errors = {};
+							if (!values.email) {
+								errors.email = 'Please enter valid email';
+							} else if (
+								!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+							) {
+								errors.email = 'Invalid email address';
+							}
+							if (!values.password) {
+								errors.password = 'Please enter password';
+							}
+							return errors;
+						}}
+						onSubmit={(values, { setSubmitting }) => {
+							handleLogin(values, { setSubmitting });
+						}}>
+						{({
+							values,
+							errors,
+							touched,
+							handleChange,
+							handleBlur,
+							handleSubmit,
+							isSubmitting,
+						}) => (<form onSubmit={handleSubmit}>
+							<FormGroup>
+								<FormLabel label='Email address' />
+								<TextInput
+									type='email'
+									name='email'
+									onChange={handleChange}
+									onBlur={handleBlur}
+									value={values.email}
+								/>
+								{errors.email && touched.email && <ErrorText error={errors.email} />}
+							</FormGroup>
+							<FormGroup>
+								<FormLabel label='Password' />
+								<TextInput
+									type='password'
+									name='password'
+									onChange={handleChange}
+									onBlur={handleBlur}
+									value={values.password}
+								/>
+								{errors.password && touched.password && <ErrorText error={errors.password} />}
+							</FormGroup>
+							<div className='d-flex justify-content-between'>
+								<Link to={'/register'} className='mt-2'>
+									Forgot password?
+								</Link>
+								<Button className='btn-primary btn-success px-3' title='Login' />
+							</div>
+							<div className='d-flex justify-content-start mt-3'>
+								<Link to={'/register'}>
+									Register Now
+								</Link>
+							</div>
+						</form>
+						)}
+					</Formik>
 				</CardBody>
 			</Card>
 		</div>
